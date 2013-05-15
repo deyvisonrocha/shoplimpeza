ad973e8f024884527c9d030d7d7b41d1f7b6d55e
Deyvison Rocha <deyvison@gmail.com>
2013-05-15 07:22:55 -0300

    - Organização do código com a nova formatação; - Alteração no nome do id do produto; - Implementação incompleta na edição de produtos com Versões do Produto;

--- application/controllers/ManagerProdutosController.php
+++ application/controllers/ManagerProdutosController.php
@@ -1,29 +1,28 @@
 <?php
+
 class ManagerProdutosController extends ManagerController
 {
-	
-	public function init()
+
+	public function init ()
 	{
 		parent::init();
 	}
-	
-	public function indexAction()
+
+	public function indexAction ()
 	{
 		$order_by = $this->_getParam('order_by');
 		$order_name = $this->_getParam('order_name');
 		
 		$produtosModel = new ProdutosModel();
 		
-		
 		$filters = array();
 		
 		$produtos = $produtosModel->listAll($filters);
 		
-		
 		$this->view->assign('produtos', $produtos);
 	}
-	
-	public function addAction()
+
+	public function addAction ()
 	{
 		try {
 			$name = $this->_getParam('name');
@@ -42,48 +41,60 @@ class ManagerProdutosController extends ManagerController
 				$flashMessages['error'] = 'O nome deve ser informado!';
 			}
 			
-			if (!empty($flashMessages)) {
+			if (! empty($flashMessages)) {
 				$this->_helper->flashMessenger->addMessage($flashMessages);
 				$this->_redirect('manager/produtos/form');
 			}
 			
-			$data = array('category_id' => $category_id, 'name' => $name, 'description_short' => $description_short,
-					'description' => $description, 'image' => $image, 'main' => $main);
+			$data = array(
+				'category_id' => $category_id,
+				'name' => $name,
+				'description_short' => $description_short,
+				'description' => $description,
+				'image' => $image,
+				'main' => $main
+			);
 			
 			$produtosModel = new ProdutosModel();
 			
 			$id = $produtosModel->add($data);
 			
-			if (!empty($product_versions)) {
+			if (! empty($product_versions)) {
+				
 				$produtosVersoesModel = new ProdutosVersoesModel();
 				
 				foreach ($product_versions as $product_version) {
-					$data = array();
-					$data['product_id'] = $id;
-					$data['name'] = $product_version['pv_name'];
-					$data['fragrance'] = $product_version['pv_fragrance'];
-					$data['color'] = $product_version['pv_color'];
-					$data['dilution'] = $product_version['pv_dilution'];
-					$data['packing'] = $product_version['pv_packing'];
 					
-					$produtosVersoesModel->add($data);
+					$product_version = array_filter($product_version);
+					
+					if (! empty($product_version)) {
+						$data = array();
+						$data['product_id'] = $id;
+						$data['name'] = $product_version['pv_name'];
+						$data['fragrance'] = $product_version['pv_fragrance'];
+						$data['color'] = $product_version['pv_color'];
+						$data['dilution'] = $product_version['pv_dilution'];
+						$data['packing'] = $product_version['pv_packing'];
+						
+						$produtosVersoesModel->add($data);
+					}
 				}
 			}
 			
 			$flashMessages['success'] = "Produto adicionado com sucesso!";
-		} catch (Zend_Exception $e) {
+		}
+		catch (Zend_Exception $e) {
 			$flashMessages['error'] = "Ocorreu um erro: " . $e->getTraceAsString();
 		}
 		
 		$this->_helper->flashMessenger->addMessage($flashMessages);
 		$this->_redirect('manager/produtos/index');
 	}
-	
-	public function editAction()
+
+	public function editAction ()
 	{
-		
 		try {
-			$id = $this->_getParam('id');
+			$id = $this->_getParam('product_id');
 			$category_id = $this->_getParam('category_id');
 			$name = $this->_getParam('name');
 			$description_short = $this->_getParam('description_short');
@@ -91,49 +102,84 @@ class ManagerProdutosController extends ManagerController
 			$image = $this->_getParam('image');
 			$main = $this->_getParam('main');
 			$product_versions = $this->_getParam('product_versions');
-		
-			$data = array('name' => $name, 'parent_id' => $parent_id);
-		
-			$categoriasModel = new CategoriasModel();
+			
+			$data = array(
+				'id' => $id,
+				'category_id' => $category_id,
+				'name' => $name,
+				'description_short' => $description_short,
+				'description' => $description,
+				'image' => $image,
+				'main' => $main
+			);
+			
+			$produtosModel = new ProdutosModel();
+			
+			$result = $produtosModel->edit($data, $id);
+			
+			$produtosVersoesModel = new ProdutosVersoesModel();
+			
+			if (! empty($product_versions)) {
+				$product_versions_exists = $produtosVersoesModel->getAllByProduct($id);
 				
-			$result = $categoriasModel->edit($data, $id);
-		
+				foreach ($product_versions as $product_version) {
+					$product_version = array_filter($product_version);
+					
+					if (! empty($product_version)) {
+						$data = array();
+						$data['product_id'] = $id;
+						$data['name'] = $product_version['pv_name'];
+						$data['fragrance'] = $product_version['pv_fragrance'];
+						$data['color'] = $product_version['pv_color'];
+						$data['dilution'] = $product_version['pv_dilution'];
+						$data['packing'] = $product_version['pv_packing'];
+						
+						if ($product_versions_exists) {}
+						
+						$produtosVersoesModel->edit($data);
+					}
+				}
+			}
+			else {
+				$produtosVersoesModel->deleteAllByProduct($id);
+			}
+			
 			$flashMessages['success'] = "Produto editado com sucesso!";
-		} catch (Zend_Exception $e) {
-			$flashMessages['error'] = "Ocorreu um erro: " . $e->getTraceAsString();
+		}
+		catch (Zend_Exception $e) {
+			$flashMessages['error'] = "Ocorreu um erro: " . $e->getMessage();
 		}
 		
 		$this->_helper->flashMessenger->addMessage($flashMessages);
 		$this->_redirect('manager/produtos/index');
 	}
-	
-	public function deleteAction()
+
+	public function deleteAction ()
 	{
 		try {
 			$id = $this->_getParam('id');
-	
+			
 			$produtosModel = new ProdutosModel();
-	
+			
 			$result = $produtosModel->delete($id);
-	
+			
 			$flashMessages['success'] = "Produto deletado com sucesso!";
-		} catch (Zend_Exception $e) {
+		}
+		catch (Zend_Exception $e) {
 			$flashMessages['error'] = "Ocorreu um erro: " . $e->getTraceAsString();
 		}
-	
+		
 		$this->_helper->flashMessenger->addMessage($flashMessages);
 		$this->_redirect('manager/produtos/index');
 	}
-	
-	public function formAction()
+
+	public function formAction ()
 	{
 		$produtosModel = new ProdutosModel();
 		
 		$id = (int) $this->_getParam('id');
 		
-		$produto_versoes = array(array('pv_name' => '', 'pv_fragrance' => '', 'pv_color' => '', 'pv_dilution' => '', 'pv_packing' => ''));
-		
-		if (!empty($id)) {
+		if (! empty($id)) {
 			$produto = $produtosModel->get($id);
 			$produtosVersoesModel = new ProdutosVersoesModel();
 			
@@ -142,13 +188,23 @@ class ManagerProdutosController extends ManagerController
 			$this->view->assign('produto', $produto);
 		}
 		
+		if (empty($produto_versoes)) {
+			$produto_versoes = array(
+				array(
+					'pv_name' => '',
+					'pv_fragrance' => '',
+					'pv_color' => '',
+					'pv_dilution' => '',
+					'pv_packing' => ''
+				)
+			);
+		}
+		
 		$categoriasModel = new CategoriasModel();
 		
 		$categorias_select = $categoriasModel->getAll();
 		
-		
 		$this->view->assign('produto_versoes', $produto_versoes);
 		$this->view->assign('categorias_select', $categorias_select);
 	}
-	
 }
